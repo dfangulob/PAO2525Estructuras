2306c3adc1f84575aa49bf1fe6d0cdc63e3c8303
DANGULO <df.angulob@uea.edu.ec>
2025-06-29 23:23:39 -0500

    Agregar enunciados a los ejercicios 1, 2 y 3

--- TareaListasYPalabras/Ejercicio1.cs
+++ TareaListasYPalabras/Ejercicio1.cs
@@ -1,3 +1,4 @@
+//Ejercicio 1 Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla.
 using System;
 using System.Collections.Generic;
 
--- TareaListasYPalabras/Ejercicio2.cs
+++ TareaListasYPalabras/Ejercicio2.cs
@@ -1,3 +1,4 @@
+//2.-Escribir un programa que almacene en una lista los números del 1 al 10 y los muestre por pantalla en orden inverso separados por comas.
 using System;
 using System.Collections.Generic;
 
